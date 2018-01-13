009bfdf0763bcfdb444c136fdff75d1dd028b851
superjoefly <superjoefly@gmail.com>
2018-01-13 04:06:10 -0700

    Changed to camelCase

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 // Create cross browser requestAnimationFrame method:
-window.requestAnimationFrame = window.requestAnimationFrame
-                               || window.mozRequestAnimationFrame
-                               || window.webkitRequestAnimationFrame
-                               || window.msRequestAnimationFrame
-                               || function(f){setTimeout(f, 1000/60)}
+window.requestAnimationFrame = window.requestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.msRequestAnimationFrame ||
+function(f){setTimeout(f, 1000/60)}
 
 var bubble1 = document.getElementById('bubble1')
 var bubble2 = document.getElementById('bubble2')
 var fish = document.getElementById('fish')
 
-var scrollheight = 	document.body.scrollHeight // height of entire document
-var windowheight = window.innerHeight // height of browser window
+// height of entire document
+var scrollHeight = 	document.body.scrollHeight
+// height of browser window
+var windowHeight = window.innerHeight
 
 
-function parallaxbubbles(){
-	var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
-	var scrollamount = (scrolltop / (scrollheight-windowheight)) * 100 // get amount scrolled (in %)
-	bubble1.style.top = -scrolltop * .2 + 'px' // move bubble1 at 20% of scroll speed
-	bubble2.style.top = -scrolltop * .5 + 'px' // move bubble2 at 50% of scroll speed
-	fish.style.left = -100 + scrollamount + '%'
+function parallaxBubbles(){
+  // get number of pixels document has scrolled vertically
+	var scrollTop = window.pageYOffset
+  // get amount scrolled (in %)
+	var scrollAmount = (scrollTop / (scrollHeight-windowHeight)) * 100
+  // move bubble1 at 20% of scroll speed
+	bubble1.style.top = -scrollTop * .2 + 'px'
+  // move bubble2 at 50% of scroll speed
+	bubble2.style.top = -scrollTop * .5 + 'px'
+  // move fish relative to scroll amount
+	fish.style.left = -100 + scrollAmount + '%'
 }
 
-window.addEventListener('scroll', function(){ // on page scroll
-	requestAnimationFrame(parallaxbubbles) // call parallaxbubbles() on next available screen repaint
+// on page scroll
+window.addEventListener('scroll', function(){
+  // call parallaxbubbles() on next available screen repaint
+	requestAnimationFrame(parallaxBubbles)
 }, false)
 
-window.addEventListener('resize', function(){ // on window resize
-	var scrollamount = (scrolltop / (scrollheight-windowheight)) * 100 // get amount scrolled (in %)
-	fish.style.left = -100 + scrollamount + '%'
+// on window resize
+window.addEventListener('resize', function(){
+  // get amount scrolled (in %)
+	var scrollAmount = (scrollTop / (scrollHeight-windowHeight)) * 100
+	fish.style.left = -100 + scrollAmount + '%'
 }, false)
